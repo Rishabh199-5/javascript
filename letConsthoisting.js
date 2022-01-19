dba5644460abe0655bcae3693555dc5b7704bbe9
Rishabh <rish@adobe.com>
2022-01-19 23:27:24 +0530

    let and const hoisting

---
+++ letConsthoisting.js
@@ -0,0 +1,9 @@
+// console.log(a); //ReferenceError: cannot access 'a' before initialization
+//console.log(b);// undefined
+let a = 10;
+
+// let a = 100; // uncomment it and you will see SyntaxError: Identifier 'a' has already been declared
+// var a = 100; // uncomment it and you will see SyntaxError: Identifier 'a' has already been declared
+
+var b = 100;
+// var b = 300; // uncomment no error in case of var
