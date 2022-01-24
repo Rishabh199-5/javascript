a4ebb9868ebe2f05c6fbb71b21d3b036eb5b5765
Rishabh <rish@adobe.com>
2022-01-24 22:42:09 +0530

    interview question on closure

---
+++ interview.js
@@ -0,0 +1,19 @@
+// function x(){
+//     for(let i =1;i<=5;i++){
+//         setTimeout(function(){
+//             console.log(i);
+//         },i*1000);
+//     }
+// }
+
+function x(){
+    for(let i =1;i<=5;i++){
+       function close(i){
+           setTimeout(function(){
+               console.log(i);
+           },i*1000);
+       }
+       close(i);
+    }
+}
+x()
