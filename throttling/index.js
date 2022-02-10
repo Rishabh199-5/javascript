11064e3c1e002be328e6a7359eb52b4176029f67
Rishabh <rish@adobe.com>
2022-02-10 23:59:23 +0530

    throttling func implementation in js

--- throttling/index.js
+++ throttling/index.js
@@ -2,3 +2,17 @@
 
 // throttling: the time diff btw two calls is delay
 // debounce: the delay diff btw two events
+
+const throttle = (fn,delay) => {
+   let flag = true;
+    return function(){
+        let context = this,args = arguments;
+        if(flag){
+            fn.apply(context,args);
+            flag = false;
+        }
+        setTimeout(()=>{
+            flag = true;
+        },delay);
+    }
+}
